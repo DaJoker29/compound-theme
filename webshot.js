4e4060219593a4ede0c30f3bc1b7cae019b8fe85
Dewitt Buckingham <dj729@comcast.net>
2014-03-02 21:45:16 -0800

    fixed screenshots...hopefully

--- webshot.js
+++ webshot.js
@@ -13,5 +13,5 @@ var mobOptions = {
 	userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'	
 }
 
-webshot('localhost/compound-theme/public/index.html', 'homepage.png', fsOptions, function(err) {});
-webshot('localhost/compound-theme/public/index.html', 'mobile.png', mobOptions, function(err) {});
+webshot('localhost/designs/compound-muzik-design/public/index.html', 'homepage.png', fsOptions, function(err) {});
+webshot('localhost/designs/compound-muzik-design/public/index.html', 'mobile.png', mobOptions, function(err) {});
