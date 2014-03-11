7b816e7b0a332c982722a6b9203eb311c9797e44
Dewitt Buckingham <dj729@comcast.net>
2014-03-10 19:04:01 -0700

    added jPlayer

---
+++ public/js/jPlayer.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#jquery_jplayer_1").jPlayer ({
+        ready: function() {
+            $(this).jPlayer("setMedia", {
+                mp3: "audio/lazy.mp3"
+            });
+        },
+    });
+
+
+    var myPlaylist = new jPlayerPlaylist ({
+        jPlayer: "#jplayer_jquery_1",
+        cssSelectorAncestor: "#jp_container_1"
+    }, [
+        {
+            title:"Lazy Day",
+            artist: "TZ Montana",
+            mp3:"audio/lazy.mp3"
+        }
+    ], {
+        playlistOptions: {
+            enableRemoveControls: true,
+            autoPlay: true
+        }
+    });
+});
