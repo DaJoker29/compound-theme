92131d67ea2020b1b983e4cf9e1f6e1c0317c24a
Dewitt Buckingham <dj729@comcast.net>
2014-03-19 01:07:20 -0700

    add store nav interactivity (filtering)

--- src/js/ajaxify.js
+++ src/js/ajaxify.js
@@ -19,11 +19,52 @@ $(document).ready(function() {
     //store link
     $('#store').click(function() {
         onlyOneSelected();
-        $('#content').load('store.html #content > *');    
+        $('#content').load('store.html #content > *', function() {
+            //Callback to add default selected for store nav
+            $('#s-all').addClass('selected').click(noFilter);
+            $('#s-muzik').click(muzikFilter);
+            $('#s-apparel').click(apparelFilter);
+            $('#s-video').click(videoFilter);
+        });    
         $('#store').addClass('selected');
     }); 
 
+    function onlyOneSelectedStore() {
+        $('.navlinks .selected').removeClass('selected'); 
+    }
+
     function onlyOneSelected() {
         $('.selected').removeClass('selected'); 
+    }
+
+    function removeFilters() {
+        $('#store-content .product').show();
+    }
+
+    function muzikFilter() {
+        onlyOneSelectedStore();
+        removeFilters();
+        $('#s-muzik').addClass('selected');
+        $('#store-content .product:not(.muzik)').hide();
+    }
+    
+    function apparelFilter() {
+        onlyOneSelectedStore();
+        removeFilters();
+        $('#s-apparel').addClass('selected');
+        $('#store-content .product:not(.apparel)').hide();
+    }
+    
+    function videoFilter() {
+        onlyOneSelectedStore();
+        removeFilters();
+        $('#s-video').addClass('selected');
+        $('#store-content .product:not(.video)').hide();
+    }
+    
+    function noFilter() {
+        onlyOneSelectedStore();
+        $('#s-all').addClass('selected');
+        removeFilters();
     }
 });
